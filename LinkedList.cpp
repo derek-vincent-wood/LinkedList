dfc47a1fb00f35b3030f3f8fa274a69f7195d277
derek-vincent-wood <84251104+derek-vincent-wood@users.noreply.github.com>
2021-05-20 14:16:48 -0400

    Initial set up

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -12,7 +12,7 @@ void DoublyLinkedList :: append(int value) //appends a value to the end of the l
 	else
 	{
 		ListNode *newNode = new ListNode(endNode, NULL, value); //create a new node who's previous node is the current end node of the list, and who's next node is NULL
-		endNode->setNextNode(newNode, isReversed); //update the link of the current end node to be the new node 
+		endNode->setNextNode(newNode, isReversed); //update the link of the current end node to be the new node
 		endNode = newNode; //set the end node to be the new node
 	}
 	size++;
@@ -30,15 +30,15 @@ void DoublyLinkedList :: insert(int position, int value) //inserts a value at th
 		{
 			append(value);
 		}
-		else //otherwise, create a new node with the value, setting its previous node to NULL and its next node to the current start node. Set the current start node's previous node to the new node. Update the new start node to be the new node 
+		else //otherwise, create a new node with the value, setting its previous node to NULL and its next node to the current start node. Set the current start node's previous node to the new node. Update the new start node to be the new node
 		{
-			ListNode *newNode = new ListNode(NULL, startNode, value); //create new node. Previous node is NULL, next node is the start node 
-			startNode -> setPrevNode(newNode, isReversed); //set the start node's previous node to be the new node 
-			startNode = newNode; //set the start node to be the new node 
-			size++; //increment the size 
+			ListNode *newNode = new ListNode(NULL, startNode, value); //create new node. Previous node is NULL, next node is the start node
+			startNode -> setPrevNode(newNode, isReversed); //set the start node's previous node to be the new node
+			startNode = newNode; //set the start node to be the new node
+			size++; //increment the size
 		}
 	}
-	else if (position == size) //if the element is being added after the last element of the list, use append to add it to the end of the list 
+	else if (position == size) //if the element is being added after the last element of the list, use append to add it to the end of the list
 	{
 		append(value);
 	}
@@ -57,7 +57,7 @@ void DoublyLinkedList :: insert(int position, int value) //inserts a value at th
 		previousNode -> setNextNode(newNode, isReversed);
 		curNode -> setPrevNode(newNode, isReversed);
 		size += 1;
-		
+
 	}
 }
 int DoublyLinkedList :: pop() //removes the value at the end of the list and returns it
@@ -67,22 +67,22 @@ int DoublyLinkedList :: pop() //removes the value at the end of the list and ret
 		cout << "invalid pop: empty list" << endl; //print a warning
 		throw "invalid pop: empty list"; //throw the error
 	}
-	if (size == 1) //if there is only one node in the list, then the statNode and endNode need to be reset 
+	if (size == 1) //if there is only one node in the list, then the statNode and endNode need to be reset
 	{
-		int nodeValue = startNode -> getValue(); //store the value of the endnode 
-		delete startNode; //delete the node 
-		startNode = NULL; //set the startNode to NULL 
+		int nodeValue = startNode -> getValue(); //store the value of the endnode
+		delete startNode; //delete the node
+		startNode = NULL; //set the startNode to NULL
 		endNode = NULL; //set the endNode to NULL
 		size --; //decrement size
-		return nodeValue; //return the value 
+		return nodeValue; //return the value
 	}
-	else //generic case, only the end node needs to be adjusted 
+	else //generic case, only the end node needs to be adjusted
 	{
-		int nodeValue = endNode -> getValue(); //store the value of the end node 
-		ListNode *newEnd = endNode -> getPrevNode(isReversed); //the new end node is the node prior to the end node 
-		delete endNode; //delete the existing end node 
-		endNode = newEnd; //set the end of the list to the new end node 
-		endNode -> setNextNode(NULL, isReversed); //link the new end node's next node to NULL 
+		int nodeValue = endNode -> getValue(); //store the value of the end node
+		ListNode *newEnd = endNode -> getPrevNode(isReversed); //the new end node is the node prior to the end node
+		delete endNode; //delete the existing end node
+		endNode = newEnd; //set the end of the list to the new end node
+		endNode -> setNextNode(NULL, isReversed); //link the new end node's next node to NULL
 		size--; //decrement size
 		return nodeValue; //return the value
 	}
@@ -97,17 +97,17 @@ void DoublyLinkedList :: del(int position) //deletes a value at the specified lo
 	}
 	if (position == size - 1) //if the element being deleted is at the end of the list, call the pop() function. Also handles the case where the start of the list is being deleted and there is only one node in the list
 	{
-		pop(); 
+		pop();
 	}
 	else if (position == 0) //if the element being deleted is at the start of the list
 	{
 		//if the start node of the list is not the only element, then the node following it must become the new start node. This is done by setting the startNode's nextNode to be the new startNode, and having that node's previousNode be set to NULL
-		ListNode *newStart = startNode -> getNextNode(isReversed); //acquire new start node 
+		ListNode *newStart = startNode -> getNextNode(isReversed); //acquire new start node
 		delete startNode; //delete the existing start node;
 		startNode = newStart; //set the startNode equal to the new start node
 		startNode -> setPrevNode(NULL, isReversed); //set the previous node of the start node to be NULL
 		size--;
-		
+
 	}
 	else //if the element being deleted is somewhere in the middle of the list, then the node prior to it needs to be linked to the node after it, and the node after it needs to be linked to the node prior to it, then the node can be deleted and the size of the list decremented
 	{
@@ -125,15 +125,15 @@ void DoublyLinkedList :: del(int position) //deletes a value at the specified lo
 		delete curNode;
 		size--;
 	}
-} 
-int DoublyLinkedList :: getVal(int position) //returns the value at the specified location within the list 
+}
+int DoublyLinkedList :: getVal(int position) //returns the value at the specified location within the list
 {
 	if (position >= size || position < 0) //if the position is greater than or equal to the size of the list, or if the position is less than 0, then it is out of bounds
 	{
 		cout << "invalid element acccess: out of bounds" << endl; //print a warning
 		throw "invalid element access: out of bounds";
 	}
-	
+
 	int curIndex = 0;
 	ListNode *curNode = startNode;
 	while (curIndex != position)
@@ -143,14 +143,14 @@ int DoublyLinkedList :: getVal(int position) //returns the value at the specifie
 	}
 	return curNode -> getValue();
 }
-void DoublyLinkedList :: setVal(int position, int value) //sets the value at an existing position in the list to the specified value 
+void DoublyLinkedList :: setVal(int position, int value) //sets the value at an existing position in the list to the specified value
 {
 	if (position >= size || position < 0) //if the position is greater than or equal to the size of the list, or if the position is less than 0, then it is out of bounds
 	{
 		cout << "invalid element acccess: out of bounds" << endl; //print a warning
 		throw "invalid element access: out of bounds";
 	}
-	
+
 	int curIndex = 0;
 	ListNode *curNode = startNode;
 	while (curIndex != position)
@@ -202,7 +202,7 @@ int main()
 	test.printer();
 	//cout << test.pop() << " | "; uncomment these two lines to make the program throw an error for trying to pop from an empty list
 	//test.printer();
-	
+
 	cout << endl << endl << endl;
 	//test deleting the first node of the list until the list is empty
 	test.append(0);
@@ -210,7 +210,7 @@ int main()
 	test.append(2);
 	test.append(3);
 	test.printer();
-	
+
 	test.del(0);
 	test.printer();
 	test.del(0);
@@ -219,7 +219,7 @@ int main()
 	test.printer();
 	test.del(0);
 	test.printer();
-	
+
 	cout << endl << endl << endl;
 	//test deleting arbitrary middle nodes
 	test.append(0);
@@ -255,7 +255,7 @@ int main()
 	cout << test.getVal(0) << "," << test.getVal(1) << "," << test.getVal(2) << "," << test.getVal(3) << "," << test.getVal(4) << "," << test.getVal(5) << endl;
 	//test.getVal(-1); //test on bounds, uncomment to throw an error
 	//test.getVal(6);
-	
+
 	//test element setting
 	cout << endl << endl << endl;
 	test.setVal(0, -1);
@@ -265,16 +265,16 @@ int main()
 	test.setVal(4, -5);
 	test.setVal(5, -6);
 	test.printer();
-	
-	//test adding an element to the start of the list using insert, clearing the list via the clear function, and building it up via the insert function 
+
+	//test adding an element to the start of the list using insert, clearing the list via the clear function, and building it up via the insert function
 	cout << endl << endl << endl;
 	test.insert(0, 0);
 	test.printer();
 	test.clear();
 	test.printer();
-	
+
 	cout << endl << endl << endl << 276 << endl;;//test building up the list by using insert, then adding an element at the end of the list using insert, then adding elements at arbitrary middle points
-	
+
 	test.insert(0, 5);
 	test.insert(0, 4);
 	test.insert(0, 3);
--- LinkedList.h
+++ LinkedList.h
@@ -5,7 +5,7 @@
 using namespace std;
 class ListNode
 {
-	
+
 public:
 	ListNode(ListNode *prev, ListNode *next, int val) //constructor
 	{
@@ -16,18 +16,18 @@ public:
 	~ListNode(){}; //Destructor - No specific actions need to be taken
 	ListNode* getNextNode(bool reversed) //returns a reference to the next node
 	{
-		if (reversed) 
+		if (reversed)
 			return previousNode;
 		else
 			return nextNode;
-	} 
+	}
 	ListNode* getPrevNode(bool reversed) //returns a reference to the previous node
 	{
 		if (reversed)
 			return nextNode;
 		else
 			return previousNode;
-	} 
+	}
 	int getValue() {return nodeVal;} //returns the value of the node
 	void setValue(int value) {nodeVal = value;} //sets the value of the node
 	void setPrevNode(ListNode *node, bool reversed) //sets the previous node reference
@@ -36,14 +36,14 @@ public:
 			nextNode = node;
 		else
 			previousNode = node;
-	}  
+	}
 	void setNextNode(ListNode *node, bool reversed) //sets the next node reference
 	{
 		if (reversed)
 			previousNode = node;
 		else
 			nextNode = node;
-	} 
+	}
 private:
 	ListNode *previousNode = NULL;
 	ListNode *nextNode = NULL;
@@ -54,7 +54,7 @@ class DoublyLinkedList
 {
 public:
 	DoublyLinkedList(){}; //constructor - Currently the list starts as being empty and integers are appended to it by calling functions
-	~DoublyLinkedList() //destructor - iterates over all of the nodes, deleting them until there are no remaining nodes 
+	~DoublyLinkedList() //destructor - iterates over all of the nodes, deleting them until there are no remaining nodes
 	{
 	clear(); //call the clear function to delete all elements of the list
 	}
@@ -65,28 +65,26 @@ public:
 			ListNode* curNode = endNode;
 		else
 			ListNode* curNode = startNode;
-		
+
 		ListNode* nextNode = NULL;
 		while (curNode != NULL)
 		{
 			nextNode = curNode -> getNextNode(isReversed); //if the current node is not empty, obtain the reference to the next node that comes after it
-			delete curNode; //delete the current node 
-			curNode = nextNode; //set the current node to the next node 
+			delete curNode; //delete the current node
+			curNode = nextNode; //set the current node to the next node
 			size -= 1;
 		}
 		startNode = NULL;
 	}
-	
+
 	void append(int value); //appends a value to the end of the list
 	void insert(int position, int value); //inserts a value at the specified location within the list
-	int pop(); //removes the value at the end of the list and returns it 
-	void del(int position); //deletes a value at the specified location within the list 
-	int getVal(int position); //returns the value at the specified location within the list 
-	void setVal(int position, int value); //sets the value at an existing position in the list to the specified value 
-	void printer(); //prints out all of the values within the list 
+	int pop(); //removes the value at the end of the list and returns it
+	void del(int position); //deletes a value at the specified location within the list
+	int getVal(int position); //returns the value at the specified location within the list
+	void setVal(int position, int value); //sets the value at an existing position in the list to the specified value
+	void printer(); //prints out all of the values within the list
 	void reverse(); //reverses the list
-	ListNode *getStartNode(){}; //gets the start node
-	ListNode *getEndNode(){}; //gets the end node 
 private:
 	ListNode* startNode = NULL;
 	ListNode* endNode = NULL;
